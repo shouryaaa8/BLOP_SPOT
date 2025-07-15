aa0ec1040ffb3dcf21b2c5aa368a5e6bd78107cb
shourya thakur <shouryathakur303@gmail.com>
2025-07-15 19:52:22 +0530

    done finally

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect('mongodb://127.0.0.1:27017/miniproject');
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const userSchema = mongoose.Schema({
     name:String,
